105282caafe8e073eb9f36f72fe8268dba3e19e9
Abishek R <90956304+Abishek0710@users.noreply.github.com>
2023-09-17 13:13:15 +0530

    Add files via upload
    First commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--name", "-n", default="Abishek", type=str)
+    args.add_argument("--age", "-a", default=21.0, type=float)
+    parse_args=args.parse_args()
+
+
+
+    print(parse_args.name,parse_args.age)
